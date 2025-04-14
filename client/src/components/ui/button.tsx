97fb7acd1ef74d34059db87f7c85bd5261466177
Suphal Bochkar <suphalbochkar@gmail.com>
2025-04-14 10:38:42 +0530

    Fix: Linting Errors and Server Configuration

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -37,6 +37,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    console.log(asChild);
     return (
       <button
         className={cn(buttonVariants({ variant, size, className }))}
